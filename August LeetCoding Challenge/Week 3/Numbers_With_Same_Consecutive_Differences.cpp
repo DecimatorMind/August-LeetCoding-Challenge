95f1884e5a5c1480ccb375dbcced526e24a4db2d
Pranjal Bhardwaj <Decimatormind@gmail.com>
2020-08-18 20:29:23 +0530

    Create Numbers_With_Same_Consecutive_Differences.cpp

---
+++ August LeetCoding Challenge/Week 3/Numbers_With_Same_Consecutive_Differences.cpp
@@ -0,0 +1,36 @@
+//
+//  Numbers_With_Same_Consecutive_Differences.cpp
+//  August LeetCoding Challenge
+//
+//  Created by Pranjal Bhardwaj on 18/08/20.
+//  Copyright © 2020 Pranjal Bhardwaj. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+using namespace std;
+vector<int> ans;
+void rcheck(int num,int k,int n){
+    if(n==1){
+        ans.push_back(num);
+        return;
+    }
+    if(num%10-k>=0){
+        rcheck(num*10+(num%10-k),k,n-1);
+    }
+    if(k){
+        if(num%10+k<10){
+            rcheck(num*10+(num%10+k),k,n-1);
+        }
+    }
+}
+int main() {
+    int n {3},k {7};
+    for(int i=1;i<10;i++){
+        rcheck(i,k,n);
+    }
+    for (auto i : ans){
+        cout << i << endl;
+    }
+    return 0;
+}
